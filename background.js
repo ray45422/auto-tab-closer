39b9942edf819c325d7a5728017545255908da46
tanitta <ttata.trit@gmail.com>
2019-04-21 19:11:45 +0900

    Cosmetic changes.

--- background.js
+++ background.js
@@ -7,13 +7,25 @@
       let alarmName = alarmNamePrefix + tabId;
       let isTargetUrl = tab.url.includes("https://twitter.com/");
       if(isTargetUrl){
-        chrome.alarms.create(alarmName, { "periodInMinutes": periodInMinutes });
-        alarmDict[alarmName] = {url: tab.url, tabId:tabId};
+        if(alarmDict[alarmName]){
+          if(tab.url != alarmDict[alarmName].url){
+            chrome.alarms.create(alarmName, { "periodInMinutes": periodInMinutes });
+            alarmDict[alarmName] = { url: tab.url, tabId: tabId };
+          }
+        }else{
+          chrome.alarms.create(alarmName, { "periodInMinutes": periodInMinutes });
+          alarmDict[alarmName] = { url: tab.url, tabId: tabId };
+        }
       }else{
         chrome.alarms.clear(alarmName);
         delete alarmDict[alarmName];
       }
+  });
 
+  chrome.tabs.onRemoved.addListener(function(tabId, changeInfo) {
+      let alarmName = alarmNamePrefix + tabId;
+      chrome.alarms.clear(alarmName);
+      delete alarmDict[alarmName];
   });
 
   chrome.alarms.onAlarm.addListener(function (alarm) {
